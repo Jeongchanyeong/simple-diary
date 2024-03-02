f1a72e6c6966a22a65ab784ca5e5ec4d70340d43
정찬영 <q135794268020@gmail.com>
2024-03-02 19:13:46 +0900

    Feat/ react-router 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import Main from './Main';
+
+import { Route, Routes } from 'react-router-dom';
+
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
@@ -9,16 +15,44 @@ body{
   display: flex;
   align-items: center;
   justify-content: center;
-  
-  background-color: #202020;
+  background-color: #a0a0a0;
 }
 `;
 
+// 상태 관리 초깃값은 DiaryDetail에, DiaryList에 상태변화 값?
+
 function App() {
+  const dummyList = [
+    {
+      id: 1,
+      title: '오늘의 일기 1',
+      content: '날씨 흐림',
+      date: new Date().getTime(),
+    },
+    {
+      id: 2,
+      title: '오늘의 일기 2',
+      content: '날씨 좋음',
+      date: new Date().getTime(),
+    },
+    {
+      id: 3,
+      title: '오늘의 일기 3',
+      content: '날씨 미치겠음',
+      date: new Date().getTime(),
+    },
+  ];
+
   return (
     <>
       <GlobalStyle />
-      <Main />
+
+      <Routes>
+        <Route path="/" element={<Home dummyList={dummyList} />} />
+        <Route path="detail:id" element={<Detail />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/edit:id" element={<Edit />} />
+      </Routes>
     </>
   );
 }
