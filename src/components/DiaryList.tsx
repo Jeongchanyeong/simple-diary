73fb8c537d761fb9f231fbabc53a65614bf0dc22
정찬영 <q135794268020@gmail.com>
2024-03-02 19:14:53 +0900

    Feat/ Home 데이터 흐름 로직 구현

--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import styled from 'styled-components';
+import DiaryItem from './DiaryItem';
 
-const DiaryList = () => {
-  return <div></div>;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  background-color: #ffacac;
+
+  width: 50vw;
+  height: 600px;
+  overflow: scroll;
+`;
+
+interface dummyListProps {
+  dummyList: { id: number; title: string; content: string; date: any }[];
+}
+const DiaryList = ({ dummyList }: dummyListProps) => {
+  return (
+    <Wrapper>
+      {dummyList.map((item) => (
+        <DiaryItem key={item.id} {...item} />
+      ))}
+    </Wrapper>
+  );
 };
 
 export default DiaryList;
