a492cfcbc4e1e8b845ef40669d4867e62f691125
정찬영 <q135794268020@gmail.com>
2024-03-03 17:54:31 +0900

    Feat/ 페이지 구분

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import DiaryEditor from '../components/DiaryEditor';
-// DiaryList에서 추가 버튼을 누르면 생성하는 페이지
+import DirayDetail from '../components/DirayDetail';
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
-
   background-color: #ebebeb;
 
   width: 60vw;
   height: 100vh;
 `;
 
-const New = () => {
+const Detail = () => {
   return (
     <Wrapper>
-      <h1>생성 페이지 입니다.</h1>
-      <DiaryEditor />
+      <h1>다이어리 디테일 (내용) 페이지 입니다.</h1>
+      <DirayDetail />
     </Wrapper>
   );
 };
 
-export default New;
+export default Detail;
