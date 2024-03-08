80e2d203f000c2382a93eef57ddcda93bcc7861d
정찬영 <q135794268020@gmail.com>
2024-03-08 12:08:00 +0900

    useState -> useReducer

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,54 +1,66 @@
 import './App.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useReducer } from 'react';
 import Header from './components/Header';
 import Editor from './components/Editior';
 import List from './components/List';
 
-// Editor input에 값을 적고 +버튼을 눌렀을 때 적은 값이 List 내부 TodoItem으로 남아야함
-// List 밑에 차곡차곡 쌓여야함
-// 내가 내용으로 적은 값을 List의 searchBar에 쌓여야 함
-
-// 일단 mokData를 만들어서 반영시켜보자
-// react Components 활용해서 prop이 잘 넘어가는지, state가 등록이 되고 있는지 확인하며 개발
-// 할 수 있다.
-
-//
-
-// Editor에 적은 값이 List에 추가되도록 함
-// 상태 변화는 Editor인데 값의 추가는 List에서 되네?
-// 최상단 컴포넌트인 App에서 상태변화함수는 Editor에, 값은 List에 보내주기
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item) => {
+        return item.id === action.targetId
+          ? { ...item, isDone: !item.isDone }
+          : item;
+      });
+    case 'DELETE':
+      return state.filter((item) => item.id !== action.targetId);
+  }
+};
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(reducer, []);
+  // const [todos, setTodos] = useState([]);
+
   const idRef = useRef(0);
 
   const onCreate = (content) => {
-    // 담고있어야 할 정보
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   const onUpdate = (targetId) => {
-    // todos배열에서 내가 겨냥한 id와 targetId가 일치하는지 확인하고 일치하다면 내가 바꾸고 싶은 부분 수정
-    setTodos(
-      todos.map((todo) =>
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
-      )
-    );
+    // setTodos(
+    //   todos.map((todo) =>
+    //     todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+    //   )
+    // );
+
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId,
+    });
   };
 
   const onDelete = (targetId) => {
-    // 내가 겨냥한 id를 제외한 것들만 꺼내서 필터링 할게
-    setTodos(
-      todos.filter((todo) => {
-        return todo.id !== targetId;
-      })
-    );
+    // setTodos(
+    //   todos.filter((todo) => {
+    //     return todo.id !== targetId;
+    //   })
+    // );
+
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId,
+    });
   };
 
   return (
